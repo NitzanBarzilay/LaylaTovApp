c223b9a576c945a4d8680505af64fd5749fa2403
Shahrzada <40363696+Shahrzada@users.noreply.github.com>
2020-10-08 21:17:41 +0300

    Splash is on

--- app/src/main/java/com/example/safetogether/splash/SplashActivity.java
+++ app/src/main/java/com/example/safetogether/splash/SplashActivity.java
@@ -1,19 +1,32 @@
 package com.example.safetogether.splash;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.PersistableBundle;
 import android.view.Menu;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.safetogether.MainLoginActivity;
 import com.example.safetogether.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private static final int DELAY_IN_MILISEC = 2500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainLoginActivity.class);
+//                Intent intent = new Intent(SplashActivity.this, YourActivityHere!.class);
+                startActivity(intent);
+                finish();
+            }
+        }, DELAY_IN_MILISEC);
     }
 }
